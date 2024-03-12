cbe477506dc427272c2409f556a7565f2d6eb44d
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-03-12 17:25:18 +0100

    add colums on table db

--- database/migrations/2024_03_12_153901_create_trains_table.php
+++ database/migrations/2024_03_12_153901_create_trains_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('agency', 30);
+            $table->string('departure_station',100);
+            $table->string('arrival_station',100);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_code',30);
+            $table->tinyInteger('number_of_carriages')->unsigned();
+            $table->boolean('on_time')->nullable()->default(0);
+            $table->boolean('deleted')->nullable()->default(0);
             $table->timestamps();
         });
     }
