28990d0bd4703e6f5ece872e0c13699aedc341fa
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-03-12 18:49:55 +0100

    add logic HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,6 +11,12 @@ class HomeController extends Controller
     public function __invoke(){
 
         $trains = Train::all();
+        foreach($trains as $train){
+            $train->on_time = ($train->on_time == 0 ) ? 'No' : 'Si';
+            $train->deleted = ($train->deleted == 0 ) ? 'No' : 'Si';
+        };
+       
+
 
         return view('home', compact('trains'));
 
